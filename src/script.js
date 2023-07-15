200e1b339337323b2217d478a35ead164d755d7c
Amsebas <sebaslgr@gmail.com>
2023-07-14 20:25:11 -0500

    .

--- src/script.js
+++ src/script.js
@@ -4,3 +4,15 @@ console.log("JavaScript is ready to run");
 /* --- Karol --- */
 
 /* --- Juan --- */
+
+let listaJugadores = []
+
+function addPlayers(){
+    listaJugadores.push("nombres de jugador agregados en el input")
+}
+function eliminatePlayers(){
+    listaJugadores.pop()
+}
+
+/*-----Los datos del array se agregarian mediante un input de tipo texto 
+que ingrese los nombres de los jugadores----- */
