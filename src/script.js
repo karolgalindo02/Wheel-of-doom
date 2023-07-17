ac0ad367185f5ec2846ce2be551e92fbe327613f
jorget0rr3s <torresor.jt@gmail.com>
2023-07-17 16:14:08 -0500

    function add player ok

--- src/script.js
+++ src/script.js
@@ -1,15 +1,5 @@
 /* --- Jorge ---*/
-var playersInput = document.getElementById('players__input');
-var playersList = document.getElementById('players__list');
 
-function addPlayer (nombre, lista){
-    var player = nombre.value;
-    console.log(player)
-}
-
-document.getElementById('save').addEventListener('click', function(){
-    addPlayer(playersInput, playersList)
-});
     /* --- Karol --- */
 
     /* --- Juan --- */
