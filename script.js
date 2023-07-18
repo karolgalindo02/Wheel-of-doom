cdfa516fabbedbc261db3870d01a35785b1866ea
jorget0rr3s <torresor.jt@gmail.com>
2023-07-18 18:23:31 -0500

    feat: add function kill player

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ var playersInput = document.getElementById('players__input');
 var playersList = document.getElementsByClassName('players__list') [0];
 var players = []; // this is array of plaryers list
 
+var killButton = document.getElementById('kill__button')
+var playerContainer = document.getElementsByClassName('player__container') [0]
+
+
 function addPlayer(nombre, lista) {
     var player = nombre.value;
     console.log(player)
@@ -12,7 +16,7 @@ function addPlayer(nombre, lista) {
 
         let new_element = document.createElement('li');
         new_element.textContent = player;
-        new_element.classList.add('list_item')
+        new_element.classList.add('list_item') // class of list 'li'
         // if (lista && typeof lista.appendChild === 'function') {
             lista.appendChild(new_element);
         // } else {
@@ -25,6 +29,25 @@ function addPlayer(nombre, lista) {
     }
 }
 
+function killPlayer (playersArray, playerCont){
+    let activePlayers = [...playersArray] /// clone players array.
+    while (activePlayers.length > 1) {
+        const playerRamdom = Math.floor(Math.random() * activePlayers.length); // select player ramdom in array
+        const selectPlayer = activePlayers[playerRamdom] // name of player select 
+        activePlayers.splice(playerRamdom, 1) //Delete playerselect ramdom
+        let newNameElement = document.createElement('h4');
+        newNameElement.textContent = selectPlayer;
+        playerCont.appendChild(newNameElement);
+
+    }
+    const winingPlayer = activePlayers[0];
+
+}
+
 document.getElementById("save").addEventListener('click', function () {
     addPlayer(playersInput, playersList)
 });
+
+killButton.addEventListener('click', function () {
+    killPlayer(players, playerContainer)
+})
