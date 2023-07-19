ccde664ea1723f34d9588bc4946e9a3469d6f93f
amSebas <sebaslgr@gmail.com>
2023-07-19 18:21:56 -0500

    idea del borrado de jugadores en lista

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 console.log("JavaScript is ready to run");
 var playersInput = document.getElementById('players__input');
 var playersList = document.getElementsByClassName('players__list') [0];
-var players = []; // this is array of plaryers list
+var players = []; // this is array of players list
 
 function addPlayer(nombre, lista) {
     var player = nombre.value;
+    let eraserButton = document.createElement('button')
+    let new_element = document.createElement('li');
     console.log(player)
     if (player !== '') {
         players.push(player);
         console.log(players)
 
-        let new_element = document.createElement('li');
-        new_element.textContent = player;
+        eraserButton.classList.add('eraser')
         new_element.classList.add('list_item')
+        new_element.textContent = player;
+
         // if (lista && typeof lista.appendChild === 'function') {
             lista.appendChild(new_element);
+            lista.appendChild(eraserButton);
         // } else {
             // console.error("Error: Invalid 'lista' element.");
         // }
 
-
         //clear input
         nombre.value = '';
     }
+    eraserButton.addEventListener('click', erasePlayer(player, eraserButton))
+}
+
+function erasePlayer(player, eraserButton){
+
 }
 
 document.getElementById("save").addEventListener('click', function () {
